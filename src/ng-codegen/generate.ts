92acecd09a1ae8c842bb75d1fca4111a64cc3c87
Binarysearch <desenlace.net@gmail.com>
2020-04-20 11:18:56 +0200

    generador de codigo angular

--- src/ng-codegen/generate.ts
+++ src/ng-codegen/generate.ts
@@ -1,7 +1,16 @@
 
 var fs = require('fs');
+const path = require('path');
 
 const file = process.argv[2];
+const outputDir = process.argv[3];
+
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+} else {
+    deleteFolderRecursive(outputDir);
+    fs.mkdirSync(outputDir);
+}
 
 interface MethodDef {
     name: string;
@@ -12,19 +21,128 @@ interface MethodDef {
 }
 
 
-fs.readFile(file, {encoding: 'utf-8'}, function(err: any,data: string){
+fs.readFile(file, { encoding: 'utf-8' }, function (err: any, data: string) {
     if (!err) {
-        console.log(analize(data));
+        const files: Set<string> = new Set();
+        files.add(data);
+        analizeImports(data, './src/', files);
+
+        const dtoNames: Set<string> = new Set();
+        const interfaces: Map<string, string> = new Map();
+        files.forEach(f => {
+            const result = analizeForControllers(f, dtoNames);
+            analizeForInterfaces(f, interfaces);
+            if (result) {
+                console.log('   ');
+                console.log('   ');
+                console.log('-----------------------------------------------');
+                console.log(result.content);
+                fs.writeFileSync(outputDir + '/services.ts', result.content);
+                console.log('-----------------------------------------------');
+            }
+        });
+
+        // Eliminamos los tipos basicos
+        [
+            'string',
+            'number',
+            'boolean'
+        ].forEach(e => {
+            dtoNames.delete(e);
+        });
+
+
+        dtoNames.forEach(dtoName => {
+            console.log('-----------------------------------------------');
+            fs.writeFileSync(outputDir + '/dtos.ts', interfaces.get(dtoName));
+            console.log(interfaces.get(dtoName));
+            console.log('-----------------------------------------------');
+        });
+
     } else {
         console.log(err);
     }
 });
 
-function analize(fileContents: string): string {
-    const size = fileContents.length;
+function analizeForInterfaces(fileContents: string, interfaces: Map<string, string>) {
+    let position = 0;
+
+    while (true) {
+        const interfaceStart = fileContents.indexOf('interface ', position);
+
+        if (interfaceStart < 0) {
+            break;
+        }
+
+        const interfaceNameStart = interfaceStart + 9;
+        const interfaceNameEnd = fileContents.indexOf('{', interfaceNameStart);
+        const interfaceName = fileContents.slice(interfaceNameStart, interfaceNameEnd).trim();
+
+        let interfaceEnd = interfaceNameEnd;
+        let count = 1;
+        let char = fileContents.charAt(interfaceEnd);
+        while (count > 0) {
+            interfaceEnd++;
+            char = fileContents.charAt(interfaceEnd);
+
+            if (char === '}') {
+                count--;
+            } else if (char === '{') {
+                count++;
+            }
+        }
+
+        const interfaceCode = fileContents.slice(interfaceStart, interfaceEnd + 1);
+        interfaces.set(interfaceName, 'export ' + interfaceCode);
+
+
+        position = interfaceEnd;
+    }
+}
+
+function analizeImports(fileContents: string, currentPath: string, files: Set<string>): void {
+    let position = 0;
+
+    while (true) {
+        const importStart = fileContents.indexOf('import', position);
+        if (importStart < 0) {
+            break;
+        }
+        const importEnd = fileContents.indexOf(';', importStart);
+        const fromTokenStart = fileContents.indexOf('from', importStart);
+
+        let quoteStart = fileContents.indexOf(`"`, fromTokenStart);
+        let quoteEnd = fileContents.indexOf(`"`, quoteStart + 1);
+        if (quoteStart < 0) {
+            quoteStart = fileContents.indexOf(`'`, fromTokenStart);
+            quoteEnd = fileContents.indexOf(`'`, quoteStart + 1);
+        }
+
+        const fileName = fileContents.slice(quoteStart + 1, quoteEnd);
+
+        if (fileName.startsWith('.')) {
+
+            const file = path.join(currentPath, fileName);
+            const newPath = path.join(currentPath, fileName, '..');
+
+            const data = fs.readFileSync(file + '.ts');
+
+            files.add(data.toString());
+            analizeImports(data.toString(), newPath, files);
+
+        }
+
+        position = importEnd;
+    }
+}
+
+function analizeForControllers(fileContents: string, dtoNames: Set<string>): { content: string, name: string } | null{
     let position = 0;
 
     const controllerStart = fileContents.indexOf('@Controller', position);
+    if (controllerStart < 0) {
+        return null;
+    }
     const classNameStart = fileContents.indexOf('class', controllerStart) + 5;
     const classNameEnd = fileContents.indexOf(`{`, classNameStart);
     const className = fileContents.slice(classNameStart, classNameEnd).trim();
@@ -63,8 +181,16 @@ function analize(fileContents: string): string {
 
         // Obtenemos el tipo de retorno
         const returnTypeStart = methodSignature.indexOf(`:`, paramTypeEnd) + 1;
-        const returnType = methodSignature.slice(returnTypeStart).trim();
+        let returnType = methodSignature.slice(returnTypeStart).trim();
 
+        returnType = returnType + 'END';
+        if (returnType.includes('Observable')) {
+            returnType = returnType.replace('Observable<', '').replace('>END', '');
+        } else if (returnType.includes('Promise')) {
+            returnType = returnType.replace('Promise<', '').replace('>END', '');
+        } else {
+            returnType = returnType.replace('END', '');
+        }
 
         methods.push({
             name: methodName,
@@ -74,10 +200,16 @@ function analize(fileContents: string): string {
             returnType: returnType
         });
 
+        dtoNames.add(paramType);
+        dtoNames.add(returnType);
+
         position = methodSignatureEnd;
     }
 
-    return createAngularService(className, methods);
+    return {
+        content: createAngularService(className, methods),
+        name: className
+    };
 }
 
 function createAngularService(className: string, methods: MethodDef[]): string {
@@ -104,17 +236,25 @@ export class ${serviceName} {
 
 function createMethodCode(method: MethodDef): string {
 
-    let returnType = method.returnType + 'END';
-    if (returnType.includes('Observable')) {
-        returnType = returnType.replace('Observable<', '').replace('>END', '');
-    } else if (returnType.includes('Promise')) {
-        returnType = returnType.replace('Promise<', '').replace('>END', '');
-    } else {
-        returnType = returnType.replace('END', '');
-    }
+
 
     return `
-    public ${method.name}(${method.paramName}: ${method.paramType}): Observable<${returnType}> {
+    public ${method.name}(${method.paramName}: ${method.paramType}): Observable<${method.returnType}> {
         const path = '${method.path}';
     }`;
 }
+
+
+function deleteFolderRecursive(filePath: string) {
+    if (fs.existsSync(filePath)) {
+        fs.readdirSync(filePath).forEach((file: string) => {
+            const curPath = path.join(filePath, file);
+            if (fs.lstatSync(curPath).isDirectory()) {
+                deleteFolderRecursive(curPath);
+            } else {
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(filePath);
+    }
+};
