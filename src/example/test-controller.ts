f24334e50611380cf9834f33ad167d46663848de
Binarysearch <desenlace.net@gmail.com>
2020-04-26 12:37:37 +0200

    refactor

--- src/example/test-controller.ts
+++ src/example/test-controller.ts
@@ -14,7 +14,7 @@ export class TestController {
     @Request('hello')
     public hello(session: Session, body: ExapleBodyDto): Observable<ExampleResult> {
         console.log('session', session);
-        this.notificationService.sendNotification('users', { id: 1, prueba: 'Funciona' });        
+        this.notificationService.sendNotification('users', { id: 1, prueba: 'Funciona' });
         return of({ something: 'body -> ' + body.something });
     }
 }
